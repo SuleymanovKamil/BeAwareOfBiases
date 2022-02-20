51731cd332e32d3d9d9c627c88a7904af94ef48f
Kamil Suleimanov <38867359+krakatoor@users.noreply.github.com>
2022-02-20 22:03:17 +0300

    add shadows

--- BeAwareOfBiases/Views/Home/Home.swift
+++ BeAwareOfBiases/Views/Home/Home.swift
@@ -12,21 +12,27 @@ struct Home: View {
         NavigationView {
             VStack {
                 Rubricator()
-               
+                
+               Spacer(minLength: 100)
+                
                 ScrollView(.horizontal, showsIndicators: false) {
                         ForEach(0..<Biase.biases.count, id: \.self) { index in
-                            VStack {
+                            VStack (spacing: 50) {
                                 Text(Biase.biases[index].name)
+                                    .padding(.top, 50)
                                 
                                 Image(Biase.biases[index].image)
                                 
                                 Text(Biase.biases[index].description)
                                     .fixedSize(horizontal: false, vertical: true)
+                                
+                               Spacer()
                             }
-                            .frame(width: UIScreen.main.bounds.width)
+                            .frame(width: UIScreen.main.bounds.width - 30)
+                            .background(Color.white.cornerRadius(20).shadow(radius: 10))
                         }
+                    .padding()
                 }
-               
                 Spacer()
             }
             .navigationBarHidden(true)
